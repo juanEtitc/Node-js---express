3e52fd2c3c60a3d77bd0d968be528f24bb83b27a
Juandiegogb <juandiegar@gmail.com>
2024-11-14 00:27:54 -0500

    testing colaborate to other repo

---
+++ src/routes/router.ts
@@ -0,0 +1,12 @@
+import {Router} from "express";
+
+
+
+export const router = Router();
+
+
+router.get("/", (req,res)=>{
+
+res.send("Hola");
+
+})
--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
 import express from  "express";
-
+import {router} from "./src/routes/router"
 
 const app = express();
 
-
+app.use(router);
 app.listen(8080);
 
 
